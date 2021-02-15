7de37c0f033f7f699500f98b60dfd1c38b15a5fc
Martin Vergier <martinvergier@gmail.com>
2021-02-15 18:55:26 +0100

    remove generated files

---
+++ vfsdata/assets/vfsdata.go
@@ -0,0 +1,9 @@
+// Placeholder file to be replaced by generated code
+
+package assets
+
+import (
+	"net/http"
+)
+
+var BundledAssets http.FileSystem = http.Dir("assets")
---
+++ vfsdata/templates/vfsdata.go
@@ -0,0 +1,9 @@
+// Placeholder file to be replaced by generated code
+
+package templates
+
+import (
+	"net/http"
+)
+
+var BundledTemplates http.FileSystem = http.Dir("templates")
